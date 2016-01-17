482171c4a60fd89d97e9ae50607980cc64eca2ea
0xfireball <talur.nihal@outlook.com>
2016-01-17 20:06:25 +0000

    installed variable

--- install/3.php
+++ install/3.php
@@ -3,6 +3,7 @@
 $toWrite = '$logURL = "' . $_POST['logName'] . '";
 $contact = "' . $_POST['email'] . '";
 $idLength = ' . $_POST['idLen'] . '; ?>';
+$toWrite = $toWrite.'\n$installdone = true;';
 
 $worked = file_put_contents("../config.php", $toWrite, FILE_APPEND);
 
